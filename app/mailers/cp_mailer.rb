cd95cb6aef89f58f3a570830242b288cc850f629
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-08-24 17:59:32 -0400

    make check for weekend acceptance of offer on Monday

--- app/mailers/cp_mailer.rb
+++ app/mailers/cp_mailer.rb
@@ -18,8 +18,9 @@ class CpMailer < ApplicationMailer
     mail(to: email, subject: "Reminder for TA Position: #{@contract[:position]}")
   end
 
-  def status_email(num_accepted)
+  def status_email(num_accepted, time_elapsed)
     @num_accepted = num_accepted
+    @time_elapsed = time_elapsed
     email = get_email(ENV["HR_ADMIN_EMAIL"])
     mail(to: email, subject: "CP Alert: New Offers Accepted")
   end
--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -6,7 +6,13 @@ s.cron('0 7 * * *') do
   date = DateTime.now
   today = date.strftime("%A")
   if today != "Sunday" && today != "Saturday"
-    yesterday = date.days_ago(1)
+    if today == "Monday"
+      yesterday = date.days_ago(3)
+      time_elapsed = "3 days"
+    else
+      yesterday = date.days_ago(1)
+      time_elapsed = "24 hours"
+    end
     num_offers = 0
     Offer.all.each do |offer|
       if offer[:accept_datetime]
@@ -16,7 +22,7 @@ s.cron('0 7 * * *') do
       end
     end
     if num_offers> 0
-      CpMailer.status_email(num_offers).deliver_now
+      CpMailer.status_email(num_offers, time_elapsed).deliver_now
     end
   end
 end
